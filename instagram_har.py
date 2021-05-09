81fbc543414828b19485d94dcb9ff110a9e5656c
Gaurav Saini <metamemelord@outlook.com>
2021-05-09 14:12:35 +0530

    WIP: Adding support for reels

--- instagram_har.py
+++ instagram_har.py
@@ -2,6 +2,7 @@ import re, requests, json, os, sys, random, string
 from bs4 import BeautifulSoup
 
 GRAPHQL_QUERY_PAT = 'graphql/query'
+REELS_PAT = 'feed/reels_media'
 INSTA_HOME = 'https://www.instagram.com/'
 failed_videos = []
 
@@ -63,64 +64,77 @@ def download_media(obj, base_path):
     print("Saved", filename)
 
 
-input_file = open(os.path.abspath(sys.argv[1]))
-file_content = input_file.read()
-input_file.close()
+def main():
+    input_file = open(os.path.abspath(sys.argv[1]))
+    file_content = input_file.read()
+    input_file.close()
 
-username = re.search('\(@[^ ]*\)', file_content).group(0)[2:-1]
-print("Username:", username)
+    username = re.search('\(@[^ ]*\)', file_content).group(0)[2:-1]
+    print("Username:", username)
 
-output_dir = os.path.join(os.getcwd(), username)
-if not os.path.isdir(output_dir):
-    os.mkdir(output_dir)
-print("Output dir:", output_dir)
+    output_dir = os.path.join(os.getcwd(), username)
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+    print("Output dir:", output_dir)
 
-# Load the json content of HAR file
-request_items = json.loads(file_content)['log']['entries']
+    # Load the json content of HAR file
+    request_items = json.loads(file_content)['log']['entries']
 
-# content-type header
-html_header = {"name": "content-type", "value": "text/html; charset=utf-8"}
-# Filter all the calls that were made to the graphql API
-filtered_response = []
-for req in request_items:
-    if INSTA_HOME + username in req['request']['url'] and str(html_header) in str(req[
-            'response']['headers']).lower():
-        get_data_from_html(req, filtered_response)
-    elif GRAPHQL_QUERY_PAT in req['request']['url']:
-        filtered_response.append(
-            json.loads(req['response']['content']['text'])['data']['user'])
+    # content-type header
+    html_header = {"name": "content-type", "value": "text/html; charset=utf-8"}
+    # Filter all the calls that were made to the graphql API
+    filtered_response = []
+    for req in request_items:
+        if req['request']['method'] == 'GET':
+            if INSTA_HOME + username in req['request']['url'] and str(
+                    html_header) in str(req['response']['headers']).lower():
+                get_data_from_html(req, filtered_response)
+            elif GRAPHQL_QUERY_PAT in req['request']['url']:
+                filtered_response.append(
+                    json.loads(
+                        req['response']['content']['text'])['data']['user'])
+            elif REELS_PAT in req['request']['url']:
+                reels = json.loads(req['response']['content']['text'])['reels']
+                for reel in reels:
+                    print(json.dumps(reels[reel]))
+                    break
 
-# Filter only timeline posts
-timeline_posts = []
-for post in filtered_response:
-    if 'edge_owner_to_timeline_media' in post:
-        timeline_posts.extend(
-            map(get_node_data, post['edge_owner_to_timeline_media']['edges']))
-    # elif 'edge_web_feed_timeline' in post:
-    #     timeline_posts.extend(
-    #         map(get_node_data, post['edge_web_feed_timeline']['edges']))
+    # Filter only timeline posts
+    timeline_posts = []
+    for post in filtered_response:
+        if 'edge_owner_to_timeline_media' in post:
+            timeline_posts.extend(
+                map(get_node_data,
+                    post['edge_owner_to_timeline_media']['edges']))
+        # elif 'edge_web_feed_timeline' in post:
+        #     timeline_posts.extend(
+        #         add_custom_key(map(get_node_data, post['edge_web_feed_timeline']['edges'])), 'story')
 
-print("Total posts found:", len(timeline_posts))
+    print("Total posts found:", len(timeline_posts))
 
-raw_posts_data_file = open(os.path.join(output_dir, "posts_raw.json"), "w")
-raw_posts_data_file.write(json.dumps(timeline_posts))
-raw_posts_data_file.close()
+    raw_posts_data_file = open(os.path.join(output_dir, "posts_raw.json"), "w")
+    raw_posts_data_file.write(json.dumps(timeline_posts))
+    raw_posts_data_file.close()
 
-for post in timeline_posts:
-    if post['__typename'] == 'GraphSidecar':
-        sidecar_id = post["id"]
-        output_dir_sidecar = os.path.join(output_dir, sidecar_id)
-        if not os.path.isdir(output_dir_sidecar):
-            os.mkdir(output_dir_sidecar)
-        for sidecar_item in post["edge_sidecar_to_children"]["edges"]:
-            download_media(sidecar_item['node'], output_dir_sidecar)
-    else:
-        download_media(post, output_dir)
+    for post in timeline_posts:
+        if post['__typename'] == 'GraphSidecar':
+            sidecar_id = post["id"]
+            output_dir_sidecar = os.path.join(output_dir, sidecar_id)
+            if not os.path.isdir(output_dir_sidecar):
+                os.mkdir(output_dir_sidecar)
+            for sidecar_item in post["edge_sidecar_to_children"]["edges"]:
+                download_media(sidecar_item['node'], output_dir_sidecar)
+        else:
+            download_media(post, output_dir)
 
-if failed_videos:
-    failed_videos_log = os.path.join(output_dir, "failed.log")
-    print("There are some failed videos, please download them manually.\n%s" %
-          failed_videos_log)
-    with open(failed_videos_log, "w") as f:
-        for failed in failed_videos:
-            f.write(INSTA_HOME + '/p/' + failed + '\n')
+    if failed_videos:
+        failed_videos_log = os.path.join(output_dir, "failed.log")
+        print(
+            "There are some failed videos, please download them manually.\n%s"
+            % failed_videos_log)
+        with open(failed_videos_log, "w") as f:
+            for failed in failed_videos:
+                f.write(INSTA_HOME + '/p/' + failed + '\n')
+
+
+main()
